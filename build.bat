951d760ba3d3571a220a0be520846556efbf99c2
kubalunacek <jakub.lunacek@outlook.com>
2018-01-18 15:17:38 +0100

    Update build.bat
    Repair of build bar

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ SET NUGET=.\bin\nuget.exe
 
 start dotnet restore KBL.ExceptionManager && dotnet build KBL.ExceptionManager
 
-xcopy ".\KBL.ExceptionManager\KBL.ExceptionManager.Base\bin\Debug\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
+xcopy ".\KBL.ExceptionManager\KBL.ExceptionManager.Base\bin\Release\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
 
 FOR /r %%f IN (*.nuspec) DO (
   echo "packing... hovno"
