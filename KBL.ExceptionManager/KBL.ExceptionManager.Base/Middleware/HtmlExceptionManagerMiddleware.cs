d79a0ac0adc8cfcae171b3499bb7f387ae55b2f6
Jakub Luňáček <jakub.lunacek@cez.cz>
2018-01-18 10:38:21 +0100

    Nothing to see :) Just bad idea in HtmlExceptionManagerMiddleware.

--- KBL.ExceptionManager/KBL.ExceptionManager.Base/Middleware/HtmlExceptionManagerMiddleware.cs
+++ KBL.ExceptionManager/KBL.ExceptionManager.Base/Middleware/HtmlExceptionManagerMiddleware.cs
@@ -1,26 +1,70 @@
 ﻿using KBL.ExceptionManager.Base.Extensions;
 using KBL.ExceptionManager.Interfaces.Middleware;
+using KBL.ExceptionManager.Interfaces.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using NLog;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace KBL.ExceptionManager.Base.Middleware
 {
     public class HtmlExceptionManagerMiddleware : IExceptionManagerMiddleware
     {
-        #region Fields
-        #endregion
+        //#region Fields
+        //private readonly RequestDelegate next;
+        //private static readonly NLog.ILogger _logger = LogManager.GetCurrentClassLogger();
 
-        #region Properties
-        #endregion
+        //#endregion
 
-        #region Cstors
-        #endregion
+        //#region Properties
+        //#endregion
 
-        #region Public methods
-        #endregion
+        //#region Cstors
+        //public HtmlExceptionManagerMiddleware(RequestDelegate next)
+        //{
+        //    this.next = next;
+        //}
 
-        #region Private methods
-        #endregion  
+        //#endregion
+
+        //#region Public methods
+        //public async Task Invoke(HttpContext context /* other scoped dependencies */)
+        //{
+        //    try
+        //    {
+        //        await next(context);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        await HandleExceptionAsync(context, ex);
+        //    }
+        //}
+
+        //private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        //{
+        //    // log exception
+        //    _logger.Error(exception, "ExceptionManagerMiddleware catch ");
+        //    var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+        //    var message = "An error occurred. Please try again your action.";
+            
+        //    if (exception is ICustomException)
+        //    {
+        //        code = (exception as ICustomException).HttpStatusCode;
+        //        message = (exception as ICustomException).UserFriendlyMessage;
+        //    }
+
+        //    context.Session.Set("exceptionManagerMessage", message.ToCharArray().byte);
+        //    return;
+        //    //return next(context);
+        //}
+
+        //#endregion
+
+        //#region Private methods
+        //#endregion  
     }
 }
