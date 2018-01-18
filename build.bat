66808ee9007be06a1a79fcf6ac84398d3d1a548a
Jakub Luňáček <jakub.lunacek@cez.cz>
2018-01-18 15:00:02 +0100

    Added script for budilng nuget package.

---
+++ build.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+SETLOCAL
+SET VERSION=%1
+SET NUGET=.\bin\nuget.exe
+
+start dotnet restore KBL.ExceptionManager && dotnet build KBL.ExceptionManager
+
+xcopy ".\KBL.ExceptionManager\KBL.ExceptionManager.Base\bin\Debug\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
+
+FOR /r %%f IN (*.nuspec) DO (
+  echo "packing... hovno"
+  %NUGET% pack %%f -Version %VERSION%
+)
