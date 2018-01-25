f63195f267743b3b392a74c07888f0a49904951c
kubalunacek <jakub.lunacek@outlook.com>
2018-01-25 13:07:51 +0100

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,13 +1,6 @@
 @ECHO OFF
 SETLOCAL
-SET VERSION=%1
-SET NUGET=.\bin\nuget.exe
 
 start dotnet restore KBL.ExceptionManager && dotnet build KBL.ExceptionManager
 
 xcopy ".\KBL.ExceptionManager\KBL.ExceptionManager.Base\bin\Release\netcoreapp2.0\*.dll" ".\nuget\lib\netcoreapp2.0\" /Y
-
-FOR /r %%f IN (*.nuspec) DO (
-  echo "packing... hovno"
-  %NUGET% pack %%f -Version %VERSION%
-)
